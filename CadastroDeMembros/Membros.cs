08c80e238af9f008d7397e8ed2881d9b49da25a0
henriqueoliveira03 <luizhomartins2@gmail.com>
2025-04-10 02:54:52 -0300

    criação da classe 'Membros'

---
+++ CadastroDeMembros/Membros.cs
@@ -0,0 +1,12 @@
+﻿namespace CadastroDeMembros
+{
+    public class Membros
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+    }
+}
